e38f9e85288ed063ca4d25cdab57a8b8ef859195
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2026-01-07 08:47:22 -0600

    updating home page

--- src/pages/Designs.tsx
+++ src/pages/Designs.tsx
@@ -12,7 +12,7 @@ function Designs() {
     <div className='max-w-[100vw]'>
       <div className='max-w-370 mx-auto py-5'>
         <div className='w-[80%] mx-auto gap-5 flex flex-col'>
-          <h1 className='text-[30px] text-center'>Websites</h1>
+          <h1 className='text-[30px] text-center'>Designs</h1>
           <div className='flex flex-row justify-evenly'>
             <ProjectHolder link="https://bethelchurchofchrist.net/" title="Bethel Church of Christ" imge={Bethel} list="Wordpress" />
             <ProjectHolder link="https://magic-carousel.netlify.app/" title="Magic Carousel" imge={Flower} list="HTML, CSS, and Javascript" />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,9 @@ import Bethel from '../assets/BethelCOC.png'
 import Flower from '../assets/corina-ardeleanu-sWlxCweDzzs-unsplash.jpg'
 import BarMockup from "../assets/chocolatePics/Mockup-Chocolate-Bars.png"
 
-
-
-
+import Pig from "../assets/pig.svg"
+import Words from "../assets/Portfolio Website.png"
+import BBQSandwich from "../assets/bbq-sandwich-1.png"
 import '../index.css'
 import SectionCompo from '../components/SectionCompo'
 
@@ -16,12 +16,18 @@ function Home() {
   return (
     <div className='max-w-[100vw]'>
       <div className='max-w-370 mx-auto py-5'>
-        
+
         <div className='w-[80%] mx-auto gap-5 flex flex-col'>
           <div className='flex flex-row justify-evenly'>
             <ProjectHolder link="https://bethelchurchofchrist.net/" title="Bethel Church of Christ" imge={Bethel} list="Wordpress" />
             <ProjectHolder link="https://magic-carousel.netlify.app/" title="Magic Carousel" imge={Flower} list="HTML, CSS, and Javascript" />
             <ProjectHolder link="/Project" title="Cacao Del Sol Project" imge={BarMockup} list="Adobe Illustrator" />
+          </div>
+
+          <div className='flex flex-row justify-evenly'>
+            <ProjectHolder link="https://www.figma.com/design/NAxrwEL3HYhPI8tZXH50tZ/Portfolio-Website-Design?node-id=0-1&t=7tXiPB5qFAIEQran-1" title="Portfolio Website Design" imge={Words} list="Figma" />
+            <ProjectHolder link="https://purple-bbq-v2.netlify.app/" title="BBQ Festival Group Project" imge={Pig} list="HTML, CSS, Javascript" />
+            <ProjectHolder link="https://www.figma.com/design/AHSdeKaOWrpyvxmxQfMyaV/BBQ-Festival-Figma-File--Copy-?node-id=0-1&t=fN65eG5YiRwaTYVQ-1" title="BBQ Figma Group Project" imge={BBQSandwich} list="Figma" />
           </div>
 
 
@@ -30,7 +36,7 @@ function Home() {
           <SectionCompo title="Design Info">
             <p className="text-[18px]">These logos were designed with Adobe Illustator.</p>
             <p className="text-[18px]">I have also created mobile apps in React Native and have coded a small guided fullstack project using Express API and Prisma. I also have collaborated in a school project designing a multi-page website with Figma.</p>
-         
+
           </SectionCompo>
 
 
